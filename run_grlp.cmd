4719832c788947e5e7786699a0879cd3d5c23139
rocky <rocky.cn@foxmail.com>
2016-12-06 17:51:19 +0800

    update script

--- run_grlp.cmd
+++ run_grlp.cmd
@@ -1,1 +1,8 @@
-gulp
+if exist node_modules (
+   call gulp
+) else (
+   call npm config set registry https://registry.npm.taobao.org --global
+   call npm config set msvs_version 2015 --global
+   call npm install
+)
+
