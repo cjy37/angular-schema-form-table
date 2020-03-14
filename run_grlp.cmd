ce514426620606c55c7dfe89993f2f8f2d34324a
chenjingyuan <rocky.cn@foxmail.com>
2020-03-14 12:12:51 +0800

    1.1.3

--- run_grlp.cmd
+++ run_grlp.cmd
@@ -2,7 +2,7 @@ if exist node_modules (
    call gulp
 ) else (
    call npm config set registry https://registry.npm.taobao.org --global
-   call npm config set msvs_version 2015 --global
+   call npm config set msvs_version 2017 --global
    call npm install --dev
    call gulp
 )
